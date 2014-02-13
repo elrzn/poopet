6741a7ad8664adb1610945648cd010e58b35e401
Eric Lorenzana <self@elrzn.me>
2014-02-13 16:27:20 +0000

    Add elasticsearch

---
+++ lib/Poopet/Module/Elasticsearch.pm
@@ -0,0 +1,22 @@
+package Poopet::Module::Elasticsearch;
+use Moose;
+use namespace::autoclean;
+
+extends 'Poopet::Module';
+
+sub _build_name { 'elasticsearch' }
+
+sub _build_requirems
+{ {dirs    => [qw(~/opt)],
+   modules => [qw(Jdk)]};
+}
+
+sub _build_script
+{ <<SCRIPT
+cd ~/opt && curl -O https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.0.0.deb
+sudo dpkg -i elasticsearch-1.0.0.deb
+rm ~/opt/elasticsearch-1.0.0.deb
+SCRIPT
+}
+
+__PACKAGE__->meta->make_immutable;
