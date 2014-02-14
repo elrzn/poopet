60903734042e53674ead10bb5731803e2cc011b4
elrzn <self@elrzn.me>
2014-02-14 21:21:34 +0100

    Remove reference to name attr as it won't work

--- lib/Poopet/Module.pm
+++ lib/Poopet/Module.pm
@@ -14,7 +14,7 @@ sub _build_script { q{} }
 sub exec {
   my $self = shift;
   my $code = $self->script;
-  qx($code) or die sprintf qq(Something went wrong while installing %s\n$!), $self->name;
+  qx($code) or die qq(Something went wrong: $!);
   qx(cd ~);    # Go to home directory after every install, just in case.
 }
 
