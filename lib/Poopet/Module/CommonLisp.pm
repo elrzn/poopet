9539509c6bf7ef1955be571ec8fdcba42c654224
Eric Lorenzana <self@elrzn.me>
2014-02-14 14:17:09 +0000

    Add CommonLisp (SBCL) formula

---
+++ lib/Poopet/Module/CommonLisp.pm
@@ -0,0 +1,13 @@
+package Poopet::Module::CommonLisp;
+use Moose;
+use namespace::autoclean;
+
+extends 'Poopet::Module';
+
+sub _build_script
+{ qq{
+    sudo apt-get install sbcl
+  };
+}
+
+__PACKAGE__->meta->make_immutable;
