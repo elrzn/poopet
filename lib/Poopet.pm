3cac4ecc4dd627f94abae64a617dc4f58ef4841d
Eric Lorenzana <self@elrzn.me>
2014-02-14 10:23:56 +0000

    First version of install method, it kinda works!

--- lib/Poopet.pm
+++ lib/Poopet.pm
@@ -2,8 +2,29 @@ package Poopet;
 use Moose;
 use namespace::autoclean;
 
+with 'Poopet::Say';
+
 sub available_modules
 { map { "Poopet::Module::$_" } grep /.pm/, split /\s/, qx(ls -l ./lib/Poopet/Module);
+}
+
+sub install
+{ my ($self, @modules) = @_;
+  my @available_modules = $self->available_modules;
+  my @req_dirs;
+  my @req_modules;
+  for my $m (@modules)
+  { my $v;
+    if (ref $m eq 'ARRAY')
+    { $v = $m->[1];
+      $m = $m->[0];
+    }
+    unless (grep { "Poopet::Module::${m}.pm" eq $_ } @available_modules)
+    { $self->say("Module $m is not available and won't be installed");
+      next;
+    }
+    $self->say("Installing module '$m'", $v ? " with version '$v'" : '');
+  }
 }
 
 __PACKAGE__->meta->make_immutable;
--- script.pl
+++ script.pl
@@ -1,4 +1,9 @@
 use lib 'lib';
-use Poopet::Module::Git;
-my $git = Poopet::Module::Git->new;
-$git->exec;
+use 5.18.1;
+use Poopet;
+my $p = Poopet->new;
+
+# say 'Available modules:';
+# say $_ for $p->available_modules;
+
+$p->install('Git', 'Leiningen', ['Perl', '5.18.1']);
